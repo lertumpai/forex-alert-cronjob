8fe42e1463a7b2d2ea93a4a037ede696b70d3732
ninew <new-sorawit@outlook.com>
2021-06-01 21:56:00 +0700

    change socket time

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ cron.schedule('*/10 * * * *', async () => {
 })
 
 // start socket
-cron.schedule('*/5 * * * *', async () => {
+cron.schedule('*/1 * * * *', async () => {
   try {
     await axios.post(
       `${SERVER_URL}/jobs/startSocketProductPrice`,
